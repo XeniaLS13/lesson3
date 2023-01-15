36f9b23f47e7329cabd51ba542e173375001c4e2
XeniaLS13 <kseniavagabova@mail.ru>
2023-01-15 20:33:33 +0300

    Добавлен пример решения задачи с заменой

---
+++ Example1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Дан текст. В тексте нужно все пробелы заменить чёрточками, малень
+//кие буквы "к" заменить большими "К", а большие "С" заменить малень
+//кими "с".
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+              + "ежели бы вас послали вместо нашего милого Винценгероде,"
+              + "вы бы взяли приступом согласие прусского короля. "
+              + "Вы так красноречивы. Вы дадите мне чаю?";
+ 
+ // string s = "qwerty"
+ //             012345
+ // s[3] // r
+
+ string Replace(string text, char oldValue, char NewValue)
+ {
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{NewValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+ }
+ string newText = Replace(text, ' ', '|');
+
+ Console.WriteLine(newText);
+ Console.WriteLine();
+ newText = Replace(newText, 'к', 'К');
+ Console.WriteLine(newText);
+ Console.WriteLine();
+ newText = Replace(newText, 'С', 'с');
+ Console.WriteLine(newText);
+ 
